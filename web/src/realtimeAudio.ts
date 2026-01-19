37a764728a9e4fe30fe1dca7534087552c03e3e1
ErikCompa <erikc@mywayv.com>
2026-01-19 09:39:21 -0500

    frontend working

--- web/src/realtimeAudio.ts
+++ web/src/realtimeAudio.ts
@@ -25,7 +25,7 @@ export default class RealtimeAudio {
                                             Realtime Transcript: 
                                             <span class="final"> ${this.final} </span>
                                             <span class="incoming">${this.incoming}</span> 
-                                            </div>`;
+                                          </div>`;
   }
 
   public clear(): void {
@@ -46,6 +46,58 @@ export default class RealtimeAudio {
       return oldText;
     }
 
-    return "hello there";
+    // same phrase
+    if (oldText === newText) {
+      return oldText;
+    }
+
+    // partial overlap
+    // old text completly inside new text -> return new text
+    if (newText.includes(oldText)) {
+      return newText;
+    }
+
+    // new text completly inside old text -> return old text
+    if (oldText.includes(newText)) {
+      return oldText;
+    }
+
+    let oldWords = oldText.split(" ");
+    let newWords = newText.split(" ");
+
+    let final = "";
+    let foundOverlap = false;
+    // overlap at the end of old text and start of new text -> return old text + non-overlapping part of new text
+    for (let i = oldWords.length; i > 0; i--) {
+      let overlap = oldWords.slice(-i).join(" ");
+      if (newText.startsWith(overlap)) {
+        final = oldText + " " + newWords.slice(i).join(" ");
+        foundOverlap = true;
+      } else {
+        continue;
+      }
+    }
+    if (foundOverlap) {
+      return final.trim();
+    }
+
+    final = "";
+    foundOverlap = false;
+    // overlap at the start of old text and end of new text -> return non-overlapping part of old text + new text
+    for (let i = newWords.length; i > 0; i--) {
+      let overlap = newWords.slice(-i).join(" ");
+      if (oldText.startsWith(overlap)) {
+        final = newText + " " + oldWords.slice(i).join(" ");
+        foundOverlap = true;
+      } else {
+        continue;
+      }
+    }
+    if (foundOverlap) {
+      return final.trim();
+    }
+
+    // completly different phrases -> reutrn both appended
+    return oldText + " " + newText;
   }
 }
